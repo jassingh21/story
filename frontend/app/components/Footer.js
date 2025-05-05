18952c2ec48b68450fe8a39608a8c79194c07b29
jassingh21 <144545034+jassingh21@users.noreply.github.com>
2025-05-05 10:33:04 +0530

    Update Footer.js

--- frontend/app/components/Footer.js
+++ frontend/app/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer className="bg-gray-950 mt-10 text-white font-pacifico text-center py-6 shadow-lg">
-      <p className="text-xl md:text-2xl tracking-wide">&copy; 2024 Narrato. All Rights Reserved.</p>
+      <p className="text-xl md:text-2xl tracking-wide">&copy; 2024 StoryThread. All Rights Reserved.</p>
     </footer>
   );
 };
