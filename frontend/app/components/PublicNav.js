3bfbd79de35153c1b80e7ef0b4764e7f03ed7353
jassingh21 <144545034+jassingh21@users.noreply.github.com>
2025-05-05 10:34:31 +0530

    Update PublicNav.js

--- frontend/app/components/PublicNav.js
+++ frontend/app/components/PublicNav.js
@@ -17,7 +17,7 @@ const PublicNav = () => {
   return (
     <div className="items-center font-amaranth bg-gray-900 p-1  shadow-lg  flex justify-between px-10 py-4 sm:px-1 md:px-2 xl:px-10 border-b border-gray-800">
       <Link href={"/"} className="text-2xl sm:text-2xl md:text-3xl xl:text-4xl">
-        Narrato
+        StoryThread
       </Link>
       <div className="items-center flex space-x-4 sm:space-x-3 md:space-x-3 xl:space-x-10 text-2xl sm:text-2xl md:text-3xl xl:text-3xl">
         <Link href={'/pages/recent-stories'} className="">
